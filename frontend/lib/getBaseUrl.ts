037a14fe90268a61fdafbc25b2550ec233d7476b
Namit <127094509+Karma121221@users.noreply.github.com>
2026-01-05 12:10:53 +0530

    Initial Commit

--- frontend/lib/getBaseUrl.ts
+++
@@ -1,3 +0,0 @@
-export const getBaseUrl = (): string => {
-  return process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
-};
--- frontend/lib/types/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./auth.types";
-export * from "./profile.types";
-export * from "./vanity.types";
-export * from "./events.types";
-export * from "./makeup.types";
